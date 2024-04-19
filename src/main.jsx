e78b37f872e90012d9d8ea3e88fdef90e3d26998
Tung <ductungltt@gmail.com>
2024-04-20 00:01:03 +0700

    refactor folder structure

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
-import "./assets/scss/index.scss";
+import "./styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
